f9fea0b3ed0d7ec3fa0c2e170292346f193de447
khoonseng <khoonseng87@gmail.com>
2026-01-30 00:04:01 +0800

    Add comment for clarity on Person class instance creation

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ birth_year = input("Enter your birth year: ")
 #converting birth year to integer
 birth_year = int(birth_year)
 
+#creating an instance of Person class
 person1 = Person(name, birth_year)
 
 #calling the greet function

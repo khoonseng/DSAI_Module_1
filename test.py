f23f5182e43e3da4775910a90c974db6426799d0
khoonseng <khoonseng87@gmail.com>
2026-01-29 23:53:50 +0800

    Update greet function to include birth year and print age

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 #This is a sample python file
 
 #function to print name
-def greet(name):
+def greet(name, birth_year):
     print(f"Hello, {name}!")
+    print_age(birth_year)
+
+#function to print age based on birth year
+def print_age(birth_year):
+    current_year=2026
+    age = current_year - birth_year
+    print(f"Age: {age}")
 
 name = "KS"
-greet(name)
+birth_year = 1987
+greet(name, birth_year)
